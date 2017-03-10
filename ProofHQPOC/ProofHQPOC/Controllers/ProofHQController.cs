e0e17e542ffcec69d5c4bc779f60e6d7f4716e9d
Pawan <pawanpt200@gmail.com>
2017-03-11 00:23:13 +0530

    Proof Hq Added

---
+++ ProofHQPOC/ProofHQPOC/Controllers/ProofHQController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//using com.proofhq.soap;
+
+namespace ProofHQPOC.Controllers
+{
+    public class ProofHQController : Controller
+    {
+        // GET: ProofHQ
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public JsonResult Validate(string user,string pass)
+        {
+          com.proofhq.soap.soapService  client = new com.proofhq.soap.soapService();
+          com.proofhq.soap.SOAPLoginObject login = client.doLogin(user,pass);
+           return Json("success");
+        }
+
+    }
+}
