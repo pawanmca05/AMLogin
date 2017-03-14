28068b12b9f63789b73fc0f0bbe7cf64e2230b34
Pawan <pawanpt200@gmail.com>
2017-03-14 08:32:41 +0530

    added comments

--- ProofHQPOC/ProofHQPOC/Controllers/ProofHQController.cs
+++ ProofHQPOC/ProofHQPOC/Controllers/ProofHQController.cs
@@ -9,9 +9,13 @@ namespace ProofHQPOC.Controllers
 {
     public class ProofHQController : Controller
     {
+       com.proofhq.soap.SOAPFileObject[] allProofs;
+       com.proofhq.soap.soapService client= new com.proofhq.soap.soapService();
+       com.proofhq.soap.SOAPLoginObject loginObject;
         // GET: ProofHQ
-        public ActionResult Index()
+        public ActionResult Index(string Id)
         {
+            ViewBag.Id = Id;
             return View();
         }
 
@@ -20,11 +24,23 @@ namespace ProofHQPOC.Controllers
             return View();
         }
 
+        public JsonResult GetComments(string Id,int fileId)
+        {
+            var comments = client.getProofComments(Id, fileId);
+            return Json(comments,JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetAllProofs(string Id)
+        {
+            allProofs = client.getAllProofs(Id, 0, "", 10);
+            return Json(allProofs,JsonRequestBehavior.AllowGet);
+        }
         public JsonResult Validate(string user,string pass)
         {
-          com.proofhq.soap.soapService  client = new com.proofhq.soap.soapService();
-          com.proofhq.soap.SOAPLoginObject login = client.doLogin(user,pass);
-           return Json("success");
+            
+            loginObject = client.doLogin(user, pass);
+            //ViewBag.Id = loginObject.session;
+            return Json(loginObject.session);
         }
 
     }
